ebd7e1f4262ccd2056afbdec2cb1bc2d7bb24d49
Hayato Nakamura <hanakamu@c1r1s7.42tokyo.jp>
2025-10-30 10:59:59 +0900

    runner.py

--- proj/runner.py
+++ proj/runner.py
@@ -3,20 +3,15 @@
 Ultra-fast MVP: Text-based 'No Midnight Ramen' ADV
 Usage: python3 runner.py
 """
-import json, sys, os, time, threading
+import json, sys, os, time, threading, asyncio
 
 from animation import opening_animation
 from animation import ending_animation
+from animation import clear_screen
 from graphics import print_clock_room_0
 from graphics import display_current_time
 
 SCENARIO_FILE = os.path.join(os.path.dirname(__file__), "scenario.json")
-
-def clear_screen():
-	if os.name == 'nt':
-		os.system('cls')
-	else:
-		os.system('clear')
 
 def wait_for_start():
 	input("Press ENTER to start the game!")
@@ -54,10 +49,51 @@ def parse_duration(s):
 		return int(m) * 60 + int(s)
 	return int(s)
 
-def beep():
-	
+async def asyncio_timer(timer_sec, story):
+	await asyncio.sleep(timer_sec)
+	story.cancel()
 
-def main():
+async def asyncio_story(node_id, nodes, state):
+	try:
+		while True:
+			clear_screen()
+			if (node_id == "room_0"):
+				print_clock_room_0()
+			node = nodes.get(node_id)
+			if node is None:
+				print(f"ノード '{node_id}' が見つかりません。scenario.json を確認してください。")
+				sys.exit(1)
+	
+			more = print_node(node)
+			if not more:
+				break
+	
+			try:
+				raw = input("> ").strip()
+				if raw.lower() in ("q", "quit", "exit"):
+					print("終了します。")
+					break
+				idx = int(raw) - 1
+				choice = node["choices"][idx]
+			except (ValueError, IndexError):
+				print("番号を選んでください（終了: q）")
+				continue
+	
+			apply_effect(state, choice.get("effect"))
+			node_id = choice["next"]
+
+	except asyncio.CancelledError:
+		return node;
+
+def time_to_eat(data, nodes, last_node):
+	print("\n" + "ピピピッ, ピピピッ, ...タイマーが鳴った。")
+	end_node_id = last_node["end"]
+	clear_screen()
+	node = nodes.get(node_id)
+	more = print_node(node)
+
+
+async def main():
 	opening_animation()
 	data = load_scenario(SCENARIO_FILE)
 	node_id = data.get("start")
@@ -67,35 +103,13 @@ def main():
 
 	user_input = input("タイマーを設定してください(sec or mm:ss) : ")
 	timer_sec = parse_duration(user_input)
-	timer = threading.Timer(timer_sec, beep)
-	timer.start()
+	story = asyncio.create_task(asyncio_story(node_id, nodes, state))
+	await_timer = asyncio.create_task(asyncio_timer(timer_sec, story))
 
-	while True:
-		clear_screen()
-		if (node_id == "room_0"):
-			print_clock_room_0()
-		node = nodes.get(node_id)
-		if node is None:
-			print(f"ノード '{node_id}' が見つかりません。scenario.json を確認してください。")
-			sys.exit(1)
+	await await_timer
+	last_node = await story
 
-		more = print_node(node)
-		if not more:
-			break
-
-		try:
-			raw = input("> ").strip()
-			if raw.lower() in ("q", "quit", "exit"):
-				print("終了します。")
-				break
-			idx = int(raw) - 1
-			choice = node["choices"][idx]
-		except (ValueError, IndexError):
-			print("番号を選んでください（終了: q）")
-			continue
-
-		apply_effect(state, choice.get("effect"))
-		node_id = choice["next"]
+	time_to_eat(data, nodes, last_node)
 
 	clear_screen()
 	display_current_time(timer_sec)
@@ -109,4 +123,4 @@ def main():
 		print(f"- {k}: {v}")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
