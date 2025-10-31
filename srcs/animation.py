15d16391369259d79867ed2a8fc27a5bc5cef490
ison <ison.c.2012.tk@gmail.com>
2025-10-31 21:00:19 +0900

    fin exc rdme

--- srcs/animation.py
+++ srcs/animation.py
@@ -348,6 +348,6 @@ def ending_animation():
         sys.stdout.write("\r" + frame)  # Overwrite the current line
         sys.stdout.flush()  # Force the output to update immediately
         time.sleep(0.5)  # Time interval between frames
-
+    time.sleep(1.5)
     print("\n")
 
