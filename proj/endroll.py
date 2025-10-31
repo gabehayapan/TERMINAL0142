665d40eaaca9a60d0f15196b3d4891ec04b2b81f
ison <ison.c.2012.tk@gmail.com>
2025-10-31 18:41:29 +0900

    endroll

--- proj/endroll.py
+++ proj/endroll.py
@@ -12,19 +12,19 @@ def endroll_animation():
     frames = [
 	"""\
 
-	       4 4                      2 2 2 2 2 2 2                            kk                                                                                               
-	       4 4                   2 2             2 2         tt              kk                                                                                           
-	       4 4                   2 2             2 2       t tt t      o o   kk   k   yy   y     o o                                                                        
-	       4 4      4 4                          2 2         tt      oo   o  kk k     yy   y   oo   o                                                                             
-	       4 4      4 4                          2 2         tt   t  oo   o  kk k     yy   y   oo   o                                                                       
-	       4 4      4 4                          2 2         tt t      o o   kk   k   yy y y     o o                                                                             
-	       4 4 4 4 4 4 4 4 4 4      2 2 2 2 2 2 2                                          y                        
-	       4 4 4 4 4 4 4 4 4 4      2 2 2 2 2 2 2                                     yy y y                     
-	                4 4          2 2                                                                          
-	                4 4          2 2                                                                   
-	                4 4          2 2                                                                             
-	                4 4          2 2                                                                                
-	                4 4             2 2 2 2 2 2 2 2                                                                              
+	4 4                      2 2 2 2 2 2 2                            kk                                                                                               
+	4 4                   2 2             2 2         tt              kk                                                                                           
+	4 4                   2 2             2 2       t tt t      o o   kk   k   yy   y     o o                                                                        
+	4 4      4 4                          2 2         tt      oo   o  kk k     yy   y   oo   o                                                                             
+	4 4      4 4                          2 2         tt   t  oo   o  kk k     yy   y   oo   o                                                                       
+	4 4      4 4                          2 2         tt t      o o   kk   k   yy y y     o o                                                                             
+	4 4 4 4 4 4 4 4 4 4      2 2 2 2 2 2 2                                          y                        
+	4 4 4 4 4 4 4 4 4 4      2 2 2 2 2 2 2                                     yy y y                     
+	         4 4          2 2                                                                          
+	         4 4          2 2                                                                   
+	         4 4          2 2                                                                             
+	         4 4          2 2                                                                                
+	         4 4             2 2 2 2 2 2 2 2                                                                              
 
 	""",
 	"""\
